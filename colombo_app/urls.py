8ec8e1f829f3762b4c8d48639a683497a2f24bb2
Nadun Perera <nadun.perera@me.com>
2018-09-15 23:39:04 +1000

    initial commit with django, bootstrap and couple of views.

---
+++ colombo_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.dashboard, name='colombo-dashboard'),
+    path('users/', views.users, name='colombo-users'),
+    path('projects/', views.projects, name='colombo-projects'),
+    path('events/', views.events, name='colombo-events'),
+    path('funnels/', views.funnels, name='colombo-funnels'),
+    path('eois/', views.eois, name='colombo-eois'),
+    path('invoices/', views.invoices, name='colombo-invoices'),
+]
--- colombo_app/views.py
+++ colombo_app/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def dashboard(request):
+    return render(request, 'colombo_app/dashboard.html')
+
+
+def users(request):
+    return render(request, 'colombo_app/users.html')
+
+
+def projects(request):
+    return render(request, 'colombo_app/projects.html')
+
+
+def events(request):
+    return render(request, 'colombo_app/events.html')
+
+
+def funnels(request):
+    return render(request, 'colombo_app/funnels.html')
+
+
+def eois(request):
+    return render(request, 'colombo_app/eois.html')
+
+
+def invoices(request):
+    return render(request, 'colombo_app/invoices.html')

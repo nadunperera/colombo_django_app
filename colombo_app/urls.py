7e72654c66bb17ca04415a7ad6237d5bd88727f3
Nadun Perera <nadun.perera@me.com>
2018-09-16 20:51:53 +1000

    cleaned template directory, started on the add new user

--- colombo_app/urls.py
+++ colombo_app/urls.py
@@ -2,12 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='colombo-dashboard'),
-    path('reminders/', views.reminders, name='colombo-reminders'),
-    path('users/', views.users, name='colombo-users'),
-    path('projects/', views.projects, name='colombo-projects'),
-    path('events/', views.events, name='colombo-events'),
-    path('funnels/', views.funnels, name='colombo-funnels'),
-    path('eois/', views.eois, name='colombo-eois'),
-    path('invoices/', views.invoices, name='colombo-invoices'),
+    path('', views.dashboard, name='colombo_dashboard'),
+    path('user/', views.user, name='colombo_users'),
+    path('user/new/', views.user_new, name='colombo_new_user'),
 ]
--- colombo_app/views.py
+++ colombo_app/views.py
@@ -5,29 +5,9 @@ def dashboard(request):
     return render(request, 'dashboard.html')
 
 
-def reminders(request):
-    return render(request, 'reminders.html')
+def user(request):
+    return render(request, 'user.html')
 
 
-def users(request):
-    return render(request, 'users.html')
-
-
-def projects(request):
-    return render(request, 'projects.html')
-
-
-def events(request):
-    return render(request, 'events.html')
-
-
-def funnels(request):
-    return render(request, 'funnels.html')
-
-
-def eois(request):
-    return render(request, 'eois.html')
-
-
-def invoices(request):
-    return render(request, 'invoices.html')
+def user_new(request):
+    return render(request, 'user_new.html')
